9accbc85977766cafde09d5e09895e1c8df6505c
umardrazdevelopify <umar.draz2519@gmail.com>
2025-10-15 13:33:27 +0500

    changes doen

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="theme-color" content="#fcf6e6" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
